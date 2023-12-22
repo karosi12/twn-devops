fb08c72e771d4612af313488da7467f2f486edfe
karosi12 <adekayor@gmail.com>
2023-12-22 16:45:30 +0200

    feat: commit version update

--- jenkins/jenkins-exercises/script.groovy
+++ jenkins/jenkins-exercises/script.groovy
@@ -40,4 +40,17 @@ def buildAndPushDockerImage(){
     echo "buildAndPushDockerImage"
 }
 
+def commitVersionUpdate(){
+    withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        # git config here for the first time run
+        sh 'git config --global user.email "jenkins@example.com"'
+        sh 'git config --global user.name "jenkins"'
+        sh 'git remote set-url origin https://$USER:$PASS@https://github.com/karosi12/twn-devops.git'
+        sh 'git add .'
+        sh 'git commit -m "ci: version bump"'
+        sh 'git push origin HEAD:jenkins-jobs'
+    }
+
+}
+
 return this
