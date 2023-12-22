ed455b99219423b0e14842bb122d639f749d4a5f
karosi12 <adekayor@gmail.com>
2023-12-22 07:34:12 +0200

    feat: added groovy script

--- jenkins/jenkins-exercises/script.groovy
+++ jenkins/jenkins-exercises/script.groovy
@@ -19,4 +19,12 @@ def incrementVersion() {
 
 } 
 
+def buildAndPushDockerImage(){
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+        sh "docker build -t ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME} ."
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "docker push ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME}"
+    }
+}
+
 return this
