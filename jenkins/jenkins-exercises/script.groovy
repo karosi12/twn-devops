2d6df4481471f74cd5fb1f5ed2429ee090a9ba86
Adeyemi Kayode <adekayor@gmail.com>
2023-12-22 17:27:21 +0200

    Update script.groovy

--- jenkins/jenkins-exercises/script.groovy
+++ jenkins/jenkins-exercises/script.groovy
@@ -45,7 +45,6 @@ def commitVersionUpdate(){
         // git config here for the first time run
         sh 'git config --global user.email "jenkins@example.com"'
         sh 'git config --global user.name $USER'
-        sh 'git config --global user.password $PASS'
         sh 'git remote set-url origin https:$USER:$PASS//github.com/karosi12/twn-devops.git'
         sh 'git add .'
         sh 'git commit -m "ci: version bump"'
