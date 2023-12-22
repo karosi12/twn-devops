c7e20e797bc29b54b9231e01a77e3f4a15b38bdd
karosi12 <adekayor@gmail.com>
2023-12-22 15:43:56 +0200

    ch: docker step[wip]

--- jenkins/jenkins-exercises/script.groovy
+++ jenkins/jenkins-exercises/script.groovy
@@ -2,36 +2,32 @@ def incrementVersion() {
     echo "Increment app version..."
 
     dir("jenkins/jenkins-exercises/app/") {
-        echo"Get here now.."
-         sh "ls -la"
-        // # update application version in the package.json file with one of these release types: patch, minor or major
-        // # this will commit the version update
         sh "npm version minor"
 
         // # read the updated version from the package.json file
         def packageJson = readJSON file: 'package.json'
-        echo "BUILD_NUMBER is: ${env.BUILD_NUMBER}"
-        echo "packageJson is: ${packageJson}"
         def version = packageJson.version
-        echo "version is: ${version}"
         // # set the new version as part of IMAGE_NAME
         env.IMAGE_NAME = "$version-$BUILD_NUMBER"
         echo env.IMAGE_NAME
     }
-       /** */
-
-    //  alternative solution without Pipeline Utility Steps plugin: 
-    // def version = sh (returnStdout: true, script: "grep 'version' package.json | cut -d '\"' -f4 | tr '\\n' '\\0'")
-    // env.IMAGE_NAME = "$version-$BUILD_NUMBER"
-
+       
+    /** 
+     alternative solution without Pipeline Utility Steps plugin: 
+    def version = sh (returnStdout: true, script: "grep 'version' package.json | cut -d '\"' -f4 | tr '\\n' '\\0'")
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+    */
 } 
 
 def buildAndPushDockerImage(){
-    // withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-    //     sh "docker build -t ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME} ."
-    //     sh 'echo $PASS | docker login -u $USER --password-stdin'
-    //     sh "docker push ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME}"
-    // }
+    dir("jenkins/jenkins-exercises/app/") {
+        sh "ls -la"
+        withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+            sh "docker build -t ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME} ."
+            sh 'echo $PASS | docker login -u $USER --password-stdin'
+            sh "docker push ${DOCKER_HUB_ID}/myapp:${IMAGE_NAME}"
+        }
+    }
     echo "buildAndPushDockerImage"
 }
 
