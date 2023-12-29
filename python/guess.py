f6d05b063c2dd5f5c4013906a074fffc9ff73d93
karosi12 <adekayor@gmail.com>
2023-12-29 17:30:18 +0100

    feat: guess number(1 - 10)

--- python/guess.py
+++ python/guess.py
@@ -1,5 +1,13 @@
+from random import randrange
+
 user_input = ""
+comp_guess_number = 0
 
 while user_input != 'exit':
-    user_input = input("Enter any number from 1 to 9\n")
-    print(f" Your guess number is {user_input}")
+    user_input = int(input("Enter any number from 1 to 10\n"))
+    print(f"Your guess number is {user_input}\n")
+    comp_guess_number = randrange(1, 11) 
+    print(f"Computer guess number is: {comp_guess_number}")
+    if user_input == comp_guess_number:
+        print(f"YOU WON! 🎊")
+        user_input = 'exit'
