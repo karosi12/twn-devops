d359ca020b8cdfa02d786b79115d55524d65cc9c
karosi12 <adekayor@gmail.com>
2023-12-29 17:52:17 +0100

    feat: clean up

--- python/guess.py
+++ python/guess.py
@@ -3,11 +3,14 @@ from random import randrange
 user_input = ""
 comp_guess_number = 0
 
-while user_input != 'exit':
-    user_input = int(input("Enter any number from 1 to 10\n"))
-    print(f"Your guess number is {user_input}\n")
-    comp_guess_number = randrange(1, 11) 
-    print(f"Computer guess number is: {comp_guess_number}")
-    if user_input == comp_guess_number:
-        print(f"YOU WON! 🎊")
-        user_input = 'exit'
+try:
+    while user_input != 'exit':
+        user_input = int(input("Enter any number from 1 to 10\n"))
+        print(f"Your guess number is {user_input}\n")
+        comp_guess_number = randrange(1, 11) 
+        print(f"Computer guess number is: {comp_guess_number}")
+        if user_input == comp_guess_number:
+            print(f"YOU WON! 🎊")
+            user_input = 'exit'
+except ValueError:
+    print("Invalid input")
